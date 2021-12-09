763b27b06e44959105be06c69524d3467b4b358e
alpharalph <ralph.slabihoud@reminds.at>
2021-12-09 11:41:33 +0100

    Added person name

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -7,6 +7,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Person myperson = new Person();
+            myperson.Name = "Franz";
 
             Console.WriteLine("Hello Students!");
             Console.WriteLine(myperson.ToString());
